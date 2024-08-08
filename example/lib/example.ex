fab1a25cd83c173d8fb6c30ebe993c8cddfd0221
Jiahua Cui <jiahua@brainboxlabs.com>
2024-08-08 09:39:36 -0400

    created a const @x

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,8 +1,17 @@
 defmodule Example do
   use Application
+
+  # create a global const value that cannot be overwritten in def 
+  @x 10 #although this can be overwritten below with @x 10
+
+
   def start(_type, _args) do
-    IO.puts("hello")
+    Example.main()
     # All this allows the function to run each time we do mix run
     Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+  def main() do 
+    IO.puts(@x)
   end
 end
